f12496343d2f2a9fe278efb054f22dc63afae76b
Brice Mulot <mulotbrice2@gmail.com>
2023-02-28 16:36:48 +0100

    code mis au propre / commenter

--- app.py
+++ app.py
@@ -5,19 +5,19 @@ from speech_recognition import recognize_from_microphone, generate_prompt
 import azure.cognitiveservices.speech as speechsdk
 import uuid
 
-app = Flask(__name__)
+app = Flask(__name__) # Crée une instance de l'application Flask
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-app.config['SECRET_KEY'] = 'une-chaîne-aléatoire-et-secrète'
-speech_key = os.environ.get('SPEECH_KEY')
-speech_region = os.environ.get('SPEECH_REGION')
+openai.api_key = os.getenv("OPENAI_API_KEY") # Récupère la clé API OpenAI à partir d'une variable d'environnement
+app.config['SECRET_KEY'] = 'une-chaîne-aléatoire-et-secrète' # Configure une clé secrète pour l'application Flask
+speech_key = os.environ.get('SPEECH_KEY') # Récupère la clé de synthèse vocale à partir d'une variable d'environnement
+speech_region = os.environ.get('SPEECH_REGION') # Récupère la région de synthèse vocale à partir d'une variable d'environnement
 
-speech_synthesizers = {}
+speech_synthesizers = {} # Initialise un dictionnaire vide pour stocker les objets SpeechSynthesizer
 
-@app.route("/", methods=("GET", "POST"))
+@app.route("/", methods=("GET", "POST")) # Définit la route pour la page d'accueil
 def index():
-    if request.method == "POST":
-        question = recognize_from_microphone()
+    if request.method == "POST": # Si la méthode HTTP est POST (c.-à-d. si le formulaire a été soumis)
+        question = recognize_from_microphone() # Reconnaît la question à partir du microphone
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=generate_prompt(question),
@@ -25,8 +25,8 @@ def index():
             max_tokens=4000,
             n=1,
             stop=None
-        )
-        response_text = response.choices[0].text
+        ) # Génère une réponse à partir du modèle OpenAI
+        response_text = response.choices[0].text # Récupère le texte de la réponse
 
         speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
         audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
@@ -34,7 +34,7 @@ def index():
         speech_config.speech_synthesis_voice_name = 'fr-CH-ArianeNeural'
 
         speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
-        speech_synthesizer.speak_text_async(response_text)
+        speech_synthesizer.speak_text_async(response_text) # Utilise la synthèse vocale pour lire la réponse à haute voix
         
         # Génère une clé aléatoire pour cet objet speech_synthesizer
         key = str(uuid.uuid4())
@@ -74,19 +74,22 @@ def index():
         
     return render_template("index.html", chat=chat)
 
+# Callback pour le bouton reset
 @app.route("/reset", methods=["POST"])
 def reset():
     session.clear()
     return redirect(url_for("index"))
 
 
-
+# Callback pour le bouton stop
 @app.route("/stop", methods=["POST"])
 def stop():
+    # Récupération de speech_synthesizer
     if 'speech_synthesizer_key' in session:
         key = session['speech_synthesizer_key']
         if key in speech_synthesizers:
             speech_synthesizer = speech_synthesizers[key]
+            # Stop la synthèse vocale
             speech_synthesizer.stop_speaking()
             return redirect(url_for("index"))
 
--- static/script.js
+++ static/script.js
@@ -1,12 +1,18 @@
+// Récupère le bouton qui permet de basculer entre les modes clair et sombre
 const darkModeBtn = document.getElementById('dark-mode-btn');
+
+// Récupère l'élément body de la page
 const body = document.body;
 
+// Ajoute un événement de clic sur le bouton
 darkModeBtn.addEventListener('click', () => {
+  // Ajoute ou supprime la classe "dark-mode" à l'élément body, selon que le mode sombre est actuellement activé ou non
   body.classList.toggle('dark-mode');
+  
+  // Change le texte du bouton en fonction du mode actuel (sombre ou clair)
   if (body.classList.contains('dark-mode')) {
     darkModeBtn.innerText = 'Light Mode';
   } else {
     darkModeBtn.innerText = 'Dark Mode';
   }
 });
-
